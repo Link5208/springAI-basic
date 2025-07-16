031bdbab60f8d7e532349b4306d4368c97c3eec4
Nguyen Hoang Long <hoanglong1292004@gmail.com>
2025-07-16 15:26:00 +0700

    Load Data to Vector Store

---
+++ src/main/java/com/springai/openai_demo/DataInitializer.java
@@ -0,0 +1,32 @@
+package com.springai.openai_demo;
+
+import java.util.List;
+
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+
+@Component
+public class DataInitializer {
+
+	private VectorStore vectorStore;
+
+	/**
+	 * @param vectorStore
+	 */
+	public DataInitializer(VectorStore vectorStore) {
+		this.vectorStore = vectorStore;
+	}
+
+	@PostConstruct
+	public void init() {
+		TextReader jobListReader = new TextReader(new ClassPathResource("job_listings.txt"));
+		TokenTextSplitter tokenTextSplitter = new TokenTextSplitter(100, 100, 5, 1000, true);
+		List<Document> documents = tokenTextSplitter.split(jobListReader.get());
+	}
+}
