2ec02cd50ee187283386242e4a9ba6c7f531fe83
Nguyen Hoang Long <hoanglong1292004@gmail.com>
2025-07-16 09:07:27 +0700

    configure Memory for Chat

--- src/main/java/com/springai/openai_demo/services/OpenAiService.java
+++ src/main/java/com/springai/openai_demo/services/OpenAiService.java
@@ -1,6 +1,11 @@
 package com.springai.openai_demo.services;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.ChatMemoryRepository;
+import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +14,14 @@ public class OpenAiService {
 	private ChatClient chatClient;
 
 	public OpenAiService(ChatClient.Builder builder) {
-		chatClient = builder.build();
+
+		InMemoryChatMemoryRepository memoryRepository = new InMemoryChatMemoryRepository();
+		ChatMemory chatMemory = MessageWindowChatMemory.builder()
+				.chatMemoryRepository(memoryRepository)
+				.maxMessages(20)
+				.build();
+		MessageChatMemoryAdvisor advisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+		chatClient = builder.defaultAdvisors(advisor).build();
 	}
 
 	public ChatResponse generateAnswer(String question) {
